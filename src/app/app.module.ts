ae0a05457d1a7ae7e2b69bf39aaed1365b7d7890
arunteja m <arunteja.m@kreatio.com>
2018-03-07 15:08:00 +0530

    new commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
-
+import * as $ from 'jquery';
 
 @NgModule({
   declarations: [
